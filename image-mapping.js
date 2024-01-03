d800dba228a0f9d1e6943201e45ae62518b12cd9
Dipeshjayswal <53213676+Dipeshjayswal@users.noreply.github.com>
2024-01-03 18:01:54 +0530

    upload the project
    upload the project

---
+++ image-mapping.js
@@ -0,0 +1,139 @@
+ImageMapPro.init('#image-map-pro', {
+    "id": "92369d87-8b3f-45ee-acc7-8e1baab5c2fd",
+    "fullscreen": {
+        "enable_fullscreen_mode": true,
+        "start_in_fullscreen_mode": true
+    },
+    "objectConfig": {
+        "pageload_animation": "grow"
+    },
+    "tooltips": {
+        "tooltip_animation": "grow"
+    },
+    "zooming": {
+        "enable_zooming": true
+    },
+    "object_list": {
+        "enable_object_list": true,
+        "menu_style": "on-top"
+    },
+    "artboards": [{
+        "background_type": "image",
+        "image_url": "https://cdn.vibrantgujarat.com/public/1703857121266-Floor-Plan.jpg",
+        "width": 1600,
+        "height": 1083,
+        "use_image_size": true,
+        "children": [{
+            "id": "af2bfff3-1f5a-4f13-8914-9c6b07619502",
+            "title": "",
+            "x": 56.53061224489796,
+            "y": 46.42038316582914,
+            "static": true,
+            "default_style": {
+                "icon_size": 38,
+                "icon_svg": "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 320 512\"><!--! Font Awesome Free 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. --><path d=\"M320 144C320 223.5 255.5 288 176 288C96.47 288 32 223.5 32 144C32 64.47 96.47 0 176 0C255.5 0 320 64.47 320 144zM192 64C192 55.16 184.8 48 176 48C122.1 48 80 90.98 80 144C80 152.8 87.16 160 96 160C104.8 160 112 152.8 112 144C112 108.7 140.7 80 176 80C184.8 80 192 72.84 192 64zM144 480V317.1C154.4 319 165.1 319.1 176 319.1C186.9 319.1 197.6 319 208 317.1V480C208 497.7 193.7 512 176 512C158.3 512 144 497.7 144 480z\"/></svg>",
+                "icon_fill": "#096bb4",
+                "icon_shadow": true
+            },
+            "mouseover_style": {
+                "icon_fill": "#096bb6"
+            }
+        }, {
+            "id": "f790cf14-018d-4014-8538-16ef54b276f4",
+            "title": "Hall1",
+            "type": "rect",
+            "x": 50.75353602691539,
+            "y": 44.81621320171503,
+            "width": 10.882219968230935,
+            "height": 10.425438369696359,
+            "default_style": {
+                "opacity": 0,
+                "background_color": "#b4dbf9",
+                "background_opacity": 0.11486486486486487
+            },
+            "mouseover_style": {
+                "background_color": "#3aa6f8",
+                "background_opacity": 0.581081081081081
+            },
+            "tooltip_style": {
+                "background_color": "#3b3b3b"
+            },
+            "tooltip_content": [{
+                "type": "Heading",
+                "text": "Hall 1",
+                "heading": "h3",
+                "other": {
+                    "id": "",
+                    "classes": "",
+                    "css": ""
+                },
+                "style": {
+                    "fontFamily": "sans-serif",
+                    "fontSize": 20.8,
+                    "lineHeight": "normal",
+                    "color": "#ffffff",
+                    "textAlign": "center"
+                },
+                "boxModel": {
+                    "width": "auto",
+                    "height": "auto",
+                    "margin": {
+                        "top": 0,
+                        "bottom": 0,
+                        "left": 0,
+                        "right": 0
+                    },
+                    "padding": {
+                        "top": 10,
+                        "bottom": 10,
+                        "left": 10,
+                        "right": 10
+                    }
+                },
+                "id": "9f21ff68-ca53-48ef-bcc8-f94035210fd8"
+            }, {
+                "type": "Paragraph",
+                "text": "INTERNATIONAL/THEMATIC PAVILION",
+                "other": {
+                    "id": "",
+                    "classes": "",
+                    "css": ""
+                },
+                "style": {
+                    "fontFamily": "sans-serif",
+                    "fontSize": 12,
+                    "lineHeight": 0,
+                    "color": "#ffffff",
+                    "textAlign": "left"
+                },
+                "boxModel": {
+                    "width": "auto",
+                    "height": "auto",
+                    "margin": {
+                        "top": 0,
+                        "bottom": 0,
+                        "left": 0,
+                        "right": 0
+                    },
+                    "padding": {
+                        "top": 10,
+                        "bottom": 10,
+                        "left": 10,
+                        "right": 10
+                    }
+                },
+                "id": "2e4c533f-9aeb-44a0-9f68-8f07942eccae"
+            }],
+            "actions": {
+                "click": "follow-link",
+                "link": "https://registrationsys.in/hall1"
+            },
+            "x_image_background": 50.75353602691539,
+            "y_image_background": 44.81621320171503
+        }]
+    }],
+    "version": "6.0.4",
+    "general": {
+        "name": "Untitled"
+    }
+})
